60dc8b864fb8f207e7c3c0eaa48c048ccffa7768
sujon-ahmed-WD <sujon.mrist@gmail.com>
2025-01-21 18:23:54 +0600

    ok

---
+++ Implement_queue_using_list.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class MyQueue
+{
+public:
+    list<int>l;
+    void push(int val)
+    {
+        l.push_back(val);
+    }
+    void pop()
+    {
+         l.pop_front();
+    }
+    int front()
+    {
+       return l.front();
+    }
+    int back()
+    {
+         return l.back();
+    }
+    int size()
+    {
+        return l.size();
+    }
+    bool empty()
+    {
+        return l.empty();
+    }
+};
+int main()
+{
+    MyQueue q;
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        int val;
+        cin >> val;
+        q.push(val);
+    }
+    while (!q.empty())
+    {
+        cout << q.front() << endl;
+        q.pop();
+    }
+    return 0;
+}
